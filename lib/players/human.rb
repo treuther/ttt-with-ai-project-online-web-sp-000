3aced7ea88645468d6580864d8225fd463cb12e2
Tracy Reuther <tracyreuther@gmail.com>
2019-07-22 00:30:24 +0000

    Automatically backed up by Learn

--- lib/players/human.rb
+++ lib/players/human.rb
@@ -1,13 +1,10 @@
 module Players
   class Human < Player #Inherits all the functionality that we have in the Player module
-<<<<<<< HEAD
 
     def move(board)
       puts "Please enter your selection."
       gets.strip
     end
-
-=======
->>>>>>> 6ab2baa86f83f7d607dc02f331ba01c285e53afe
   end
+  
 end
